5b02f11e6a29c6519f7049a3955d6b6e4efdfe51
Khavkin <hdm@ukr.net>
2023-09-26 19:56:54 +0300

    fix connection error to PostgreSQL on render

--- config/postgresql/db.js
+++ config/postgresql/db.js
@@ -8,16 +8,26 @@ console.log(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_USER_PASSWORD);
 
 // const sequelize = new Sequelize(process.env.DB_URI, { ssl: false });
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_USER_PASSWORD, {
+const sequelize = new Sequelize({
+  database: DB_NAME,
+  username: DB_USER,
+  password: DB_USER_PASSWORD,
   host: DB_HOST,
   port: DB_PORT,
   dialect: "postgres",
-  ssl: false,
+  ssl: true,
   pool: {
     max: 50,
     min: 0,
     acquire: 30000,
     idle: 5000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    keepAlive: true,
   },
 });
 
